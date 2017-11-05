96d88f3a2f33de35993afadc1e49c64b629a188f
Jeremia Kimelman <jbkimelman@gmail.com>
2017-11-05 18:08:19 -0500

    Use kui-ui

--- src/queue.js
+++ src/queue.js
@@ -1,9 +1,17 @@
+const express = require('express');
 const kue = require('kue');
+const ui = require('kue-ui-express');
+
+const app = express();
 const queue = kue.createQueue({
   redis: {
     host: 'redis'
   }
 });
 
-kue.app.set('title', 'Gothamist scraping queue');
-kue.app.listen(3000);
+ui(app, '/admin/', '/api/');
+
+// app.set('title', 'Gothamist scraping queue');
+app.use('/api/', kue.app);
+
+app.listen(3000);
